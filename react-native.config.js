c11790b29ade794366a7636be48a8936778ff57c
Janit Chawla <68466788+JanitChawla@users.noreply.github.com>
2024-09-03 17:23:47 +0530

    Create react-native.config.js

---
+++ react-native.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  dependencies: {
+    ...(process.env.NO_FLIPPER // When set, skip flipper includes for iOS archive builds (release buidls)
+      ? { 'react-native-flipper': { platforms: { ios: null } } }
+      : {}),
+  }
